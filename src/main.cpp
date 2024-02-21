ff1b1408f39c44bde4945ce5aafc9c17605c6a5c
lancas17 <michael.ej.lancaster@gmail.com>
2024-02-21 11:24:59 -0500

    corrected mistake from pull request; also button press bug (wouldn't work after charging animation) seems fixed

--- src/main.cpp
+++ src/main.cpp
@@ -66,15 +66,8 @@ void setup() {
 
   // if the battery is completely drained and put on the charger, we won't get the STAT pin interrupt, so check the STAT pin here
   while (digitalRead(STAT_PIN)) {
-    if (socChanged) {
-      socChanged = false;
-      lipo.clearAlertFlag(MAX1704X_ALERTFLAG_SOC_CHANGE); // clear the alert flag in the status register
-      setConfig(); // this clears the alert bit in the config register (needed to pull the alert pin high again)
-      delay(10);
-      percentFloat = lipo.cellPercent();
-      delay(10);
-    }
-    displayChargingStatus(percentFloat);
+    if (socChanged) socGetUpdate();
+    displayChargingStatus(percent);
   }
 
   // disable ADC to save power; see https://github.com/SpenceKonde/megaTinyCore/blob/c7afbb3161086edb54112005df15e4a1db84bf16/megaavr/extras/PowerSave.md
@@ -98,26 +91,11 @@ void loop() {
       if (socChanged) socGetUpdate();
       displayChargingStatus(percent);
     }
-    uint8_t err = writeToController(REG_CONTROLLER_PERCENT, percentInt);
-    if (err == 2) { // the other device didn't acknowledge, so it's probably the charger that pulled STAT high
-      while (digitalRead(STAT_PIN)) { // continue to display the charging animation until the charger is disconnected or charging is complete (STAT will be pulled low in either case)
-        if (socChanged) {
-          socChanged = false;
-          lipo.clearAlertFlag(MAX1704X_ALERTFLAG_SOC_CHANGE); // clear the alert flag in the status register
-          setConfig(); // this clears the alert bit in the config register (needed to pull the alert pin high again)
-          delay(10);
-          percentFloat = lipo.cellPercent();
-          delay(10);
-        }
-        displayChargingStatus(percentFloat);
-      }
-    }
-    else if (err != 0) displayError(err); // since this is development code, blink out any other errors that are unexpected
     statAsserted = false;
   }
 
   // button press woke us up; display the battery SOC on the indicator LEDs
-  else if (buttonPressed) {
+  if (buttonPressed) {
     if (socChanged) {
       socGetUpdate();
     }
